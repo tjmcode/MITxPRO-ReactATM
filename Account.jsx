8b7d7a7452a4c14f25c03d69b52b2f343f62f6f5
Timothy J McGuire <tmcguire@mcode.com>
2022-05-05 11:41:55 -0400

    Refactored to MIcroCODE Standards

---
+++ Account.jsx
@@ -0,0 +1,196 @@
+// #region  H E A D E R
+// <copyright file="Account.js" company="MicroCODE Incorporated">Copyright © 2022 MicroCODE, Inc. Troy, MI</copyright><author>Timothy J. McGuire</author>
+// #region  P R E A M B L E
+// #region  D O C U M E N T A T I O N
+/*
+ *      Title:    ATM Account Component
+ *      Module:   Modules (./Account.js)
+ *      Project:  MicroCODE Common Library
+ *      Customer: Internal
+ *      Creator:  MicroCODE Incorporated
+ *      Date:     February 2022
+ *      Author:   Timothy J McGuire
+ *
+ *      Designed and Coded: 2022 MicroCODE Incorporated
+ *
+ *      This software and related materials are the property of
+ *      MicroCODE Incorporated and contain confidential and proprietary
+ *      information. This software and related materials shall not be
+ *      duplicated, disclosed to others, or used in any way without the
+ *      written of MicroCODE Incorported.
+ *
+ *
+ *      DESCRIPTION:
+ *      ------------
+ *
+ *      This module implements the MicroCODE's Common JavaScript Template.
+ *      This file is copied to start all MicroCODE JavaScript code files.
+ *
+ *
+ *      REFERENCES:
+ *      -----------
+ *
+ *      1. MIT xPRO Style Guide
+ *         https://student.emeritus.org/courses/3291/files/2554233/download?wrap=1
+ *
+ *      2. AirBnB JavaScript Style Guide
+ *         https://github.com/airbnb/javascript
+ *
+ *      3. Turing School Style Guide
+ *         https://github.com/turingschool-examples/javascript/tree/main/es5
+ *
+ *      4. MDN Web Docs - JavaScript Classes
+ *         https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+ *
+ *      5. JSDoc - How to properly document JavaScript Code.
+ *         https://
+ *
+ *      6. MicroCODE JavaScript Style Guide
+ *         Local File: MCX-S02 (Internal JS Style Guide).docx
+ *         https://github.com/MicroCODEIncorporated/JavaScriptSG
+ *
+ *
+ *      DEMONSTRATION VIDEOS:
+ *      --------------------
+ *
+ *      1. ...
+ *
+ *
+ *
+ *      MODIFICATIONS:
+ *      --------------
+ *
+ *  Date:         By-Group:   Rev:     Description:
+ *
+ *  04-May-2022   TJM-MCODE  {0001}    New module for a Bank ATM App.
+ *
+ *
+ */
+"use strict";
+
+// #endregion
+// #endregion
+// #endregion
+
+// #region  J A V A S C R I P T
+// #region  F U N C T I O N S
+
+// #region  C O N S T A N T S
+
+// #endregion
+
+// #region  P R I V A T E   F I E L D S
+
+// #endregion
+
+// #region  E N U M E R A T I O N S
+
+// #endregion
+
+// #region  M E T H O D S – P U B L I C
+
+/**
+ * Account() - handles Deposits and Withdraws into a Bank Account from an ATM.
+ *             This is the PARENT React Component.
+ *
+ * @api public
+ *
+ * @param {type} none no arguments.
+ * @returns JSX code to render the Account Component with Deposit and Withdraw controls.
+ *
+ * @example
+ *
+ *      Account();
+ *
+ */
+const Account = () =>
+{
+    //     local store,       function to update,           intial state = $0.00
+    const [transactionAmount, setTransactionAmount] = React.useState(0);
+
+    //     local store,    function to update,           intial state = $0.00
+    const [accountBalance, setAccountBalance] = React.useState(0);
+
+    //     local store,       function to update,              intial state = depositing
+    const [isDepositingFunds, setIsDepositingFunds] = React.useState(true);
+
+    let status = `Your Account Balance is: $${accountBalance}`;
+
+    console.log(`Account Rendered with isDepositingFunds: ${isDepositingFunds}`);
+
+    const handleChange = (event) =>
+    {
+        console.log(`handleChange ${event.target.value}`);
+        setTransactionAmount(Number(event.target.value));
+    };
+
+    const handleSubmit = () =>
+    {
+        let newBalance = isDepositingFunds ? accountBalance + transactionAmount : accountBalance - transactionAmount;
+
+        setAccountBalance(newBalance);
+        setTransactionAmount(0);
+        event.preventDefault();
+    };
+
+    // "AtmTransaction" is a CHILD Component of the "Account" Component...
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2 id="total">{status}</h2>
+            <br />
+            <button className="btn btn-success" onClick={() => setIsDepositingFunds(true)}>Deposit</button>
+            <button className="btn btn-warning" onClick={() => setIsDepositingFunds(false)}>Withdraw</button>
+            <br />
+            <br />
+            <AtmTransaction onChange={handleChange} isDeposit={isDepositingFunds} funds={transactionAmount}></AtmTransaction>
+        </form>
+    );
+};
+
+/**
+ * AtmTransaction() - handles Deposits and Withdraws transaction on the parent Account.
+ *                    This is a CHILD component of "Account".
+ *
+ * @api public
+ *
+ * @param {function} onChange a function in the parent component to handle changes to the funds value.
+ * @param {boolean} isDeposit a value indicating whether or not this transaction is a deposit.
+ * @param {number} finds a value representing teh value of the transaction.
+ *
+ * @returns JSX code to render the Account balance.
+ *
+ * @example
+ *
+ *      <AtmTransaction onChange={handleChange} isDeposit={isDeposit}></AtmTransaction>
+ *
+ */
+const AtmTransaction = ({onChange, isDeposit, funds}) =>
+{
+    const transactionType = ["Deposit", "Withdraw"];
+
+    console.log(`ATM isDeposit: ${isDeposit}`);
+
+    if (funds == 0)
+    {
+        funds = '';  // display nothing for ZERO
+    }
+
+    return (
+        <label className="label huge">
+            <h3> {transactionType[Number(!isDeposit)]}</h3>
+            <input className="form-control form-control" type="number" width="200" onChange={onChange} value={funds}></input>
+            <input className="btn btn-info" type="submit" width="200" value="Submit"></input>
+        </label>
+    );
+};
+
+// #region  R E A C T   E X E C U T I O N
+
+// "Account" is the PARENT React Component, rendering it creates the control elements of the UI
+// which are linked to event handlers.
+ReactDOM.render(<Account />, document.getElementById("root"));
+
+// #endregion
+
+// #endregion
+// #endregion
--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # MITxPRO-ReactATM
+
 A pure REACT App to simulate a Banking ATM.
+
+## Description
+
+This is a simple example of REACT Component application.
+This app implements a simple ATM Machine.
+
+## Getting Started
+
+
+### Dependencies
+
+* REACT
+* BABEL
+
+
+### Installing
+
+* Clone this repo
+
+* Use "npm install" to load a simple HTTP Server for the project
+```
+npm install -- global http-server
+```
+
+* Use "http-server" command in the project folder to start the HTTP Server with *no caching*.
+```
+http-server -c-1
+```
+
+* Navigate your Browser to...
+```
+localhost:8080
+```
+
+* Demonstration of app startup...
+
+<p align="left"><img src=".\app-startup.png" width="720" title="Server Startup..."></p>
+
+* Example of app u.i...
+
+<p align="left"><img src=".\app-ui.png" width="720" title="App U.I..."></p>
+
+
+## Help
+
+This is an unsupported demonstation project.
+
+## Terminology
+
+| Word or Acronym	| Description/Definition                                |
+|-------------------|-------------------------------------------------------|
+|  NPM	            | Node Package Manager, actually “Node PM”, “Node pkgmakeinst” a system to deploy, install, and maintain NodeJS Apps. (PM was a BASH utility).
+|  Template	        | A file used to start others to ensure code and documentation consistency.
+|  React            | A Web UI development system, a JavaScript library developed by Facebook and made public—and Open Source—since 2013.
+|  Babel            | A JavaScript ‘transpiler’ that compiles JavaScript containing newly released syntax into code that is backward compatible with currently available Browsers.
+
+
+## Authors
+
+Contributors names and contact info
+
+* Dr. Williams (MIT) [@Unknown](https://twitter.com/Unknown)
+
+* Timothy J McGuire [@TimothyMcGuire](https://twitter.com/TimothyMcGuire)
+
+
+## Version History
+
+* 0.1
+    * Refactored into the MicroCODE [MCODE] style and templates.
+    * See [commit change]() or See [release history]()
+* 0.0
+    * Cloned the MIT Starter Code.
+
+## Future Development
+
+* 0.2
+    * Disable withdraws when there are no funds.
+
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
